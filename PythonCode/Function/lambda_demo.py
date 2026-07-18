574266b03c7ae6bed663574ac1cf3428a6e7e203
neetesh8s <neetesh8s@gmail.com>
2026-07-18 17:46:24 +0530

    Neeetsh

---
+++ PythonCode/Function/lambda_demo.py
@@ -0,0 +1,13 @@
+
+# def square(num):
+#     return num * num
+
+fun = lambda num : num * num # anonymous function
+
+result = fun(5)
+print(result)
+
+add = lambda a,b : a + b # we can pass multiple value here
+
+result1 = add(5,6)
+print(result1)
